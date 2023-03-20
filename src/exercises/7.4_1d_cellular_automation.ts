3123173e446e290b259b473e871e7ebe72325bc9
Sam VanTassel <vantassel.sam@gmail.com>
2023-03-19 21:30:34 -0700

    feat: scrolling 1d CA

--- src/exercises/7.4_1d_cellular_automation.ts
+++ src/exercises/7.4_1d_cellular_automation.ts
@@ -6,10 +6,11 @@ export const oneDellularAutomationSketch = (p5: P5) => {
   const WIDTH = 1000;
   const HEIGHT = 1000;
 
-  const CELL_WIDTH = 5;
+  const CELL_WIDTH = 10;
   const DENSITY = .05;
   let cells: number[] = [];
   let nextCells: number[] = [];
+  let display: number[][] = [];
 
   const colorMap = {
     0: 'white',
@@ -21,9 +22,14 @@ export const oneDellularAutomationSketch = (p5: P5) => {
 
 // 111, 110, 101, 100, 011, 010, 001, 000
   generateRulesets(states);
+  const interesting =[
+  102,100, 106,110,111,90,87,76, 74, 63, 61, 58, 55,
+  46,31,26,10,103,108,115,119,122,132,136,138,146,
+  148,150,151, 154, 159,170,189,215,226,231,
+  ]
 
   let y = 0;
-  let ri = Math.floor(Math.random()* 256);
+  let ri = 0;
   let startingCells: number[];
 
   p5.setup = () => {
@@ -33,12 +39,15 @@ export const oneDellularAutomationSketch = (p5: P5) => {
     );
     const NUM_CELLS = Math.floor(p5.width/CELL_WIDTH);
     for (let i = 0; i < NUM_CELLS; i++) {
-      cells.push(p5.random() < DENSITY ? 1 : 0);
+      cells.push(
+        p5.random() < DENSITY ? 1 : 0
+        );
     };
     const middleIndex = Math.floor(p5.width / (2 * CELL_WIDTH));
-    console.log(NUM_CELLS, middleIndex);
     cells[middleIndex] = 1;
     startingCells = [...cells];
+
+    ri = interesting[Math.floor(p5.random(0, interesting.length))] -1;
     p5.frameRate(100);
     p5.noStroke();
     p5.background('white');
@@ -53,20 +62,29 @@ export const oneDellularAutomationSketch = (p5: P5) => {
       nextCells[i] = rules(left, current, right);
     });
 
-    cells.forEach((cell, x) => {
-      p5.fill(colorMap[cell] || 'blue');
-      p5.rect(x * CELL_WIDTH, y * CELL_WIDTH, CELL_WIDTH, CELL_WIDTH);
+    display.push(cells);
+    if (display.length >= p5.height/CELL_WIDTH) display = display.slice(1);
+
+    // cells.forEach((cell, x) => {
+    //   p5.fill(colorMap[cell] || 'blue');
+    //   p5.rect(x * CELL_WIDTH, y * CELL_WIDTH, CELL_WIDTH, CELL_WIDTH);
+    // });
+    // y ++;
+
+    display.forEach((row, y) => {
+      row.forEach((cell, x) => {
+        p5.fill(colorMap[cell]);
+        p5.rect(x * CELL_WIDTH, y * CELL_WIDTH, CELL_WIDTH, CELL_WIDTH);
+      });
     });
-    y ++;
   
-    if (y * CELL_WIDTH>= p5.height) {
-      y = 0;
-      cells = [...startingCells];
-      ri = (ri + 1) % r.length;
-      // p5.background('white');
-    } else {
+    // if (y * CELL_WIDTH>= p5.height) {
+    //   y = 0;
+    //   cells = [...startingCells];
+    //   ri = (ri + 1) % r.length;
+    // } else {
       cells = [...nextCells];
-    }
+    // }
   };
 
   const rules = (left: number, current: number, right: number) => {
@@ -81,7 +99,6 @@ export const oneDellularAutomationSketch = (p5: P5) => {
     // access ruleset in reverse at binary index, eg 7 for 001
     // corresponds to ruleset definition because definition is a bianry state.
     // same could be done for rules with more states with different radix values for parseInt
-    console.log(ruleset[ruleset.length - 1 - i]);
     return ruleset[ruleset.length - 1 - i];
   }
 
@@ -96,7 +113,6 @@ export const oneDellularAutomationSketch = (p5: P5) => {
 const r = [];
 const generateRulesets = (states: number[], ruleset = []) => {
   if (ruleset.length === Math.pow(states.length, 3)) {
-    console.log(ruleset);
     r.push(ruleset);
     return;
   };
