9ec91cf56a027a4f86629f66b3fb56a8a67c3cac
Sam VanTassel <vantassel.sam@gmail.com>
2023-05-23 04:24:08 -0700

    fix: sidebar & page render

--- src/app.ts
+++ src/app.ts
@@ -28,89 +28,137 @@ import {
   flockingSketch,
   rayCastingSketch,
   rayCastingPlusSketch,
-} from './exercises';
+} from "./exercises";
 import { environmentSketch } from "./environment";
 
 const sketches: [string, (p5: P5) => void][] = [
-  ['0.1 - 1D Collision', oneDimendionalCollisionSketch],
-  ['1 - Mouse Followers', mouseFollowersSketch],
-  ['1a - Triangle Collision', triangleCollisionSketch],
-  ['2.1 - Wind & Gravity', windGravitySketch],
-  ['2.2 - Wind, Gravity & Friction', windGravityFrictionSketch],
-  ['2.2b - Wind, Gravity, Friction, Collision', windGravityFrictionCollisionSketch],
-  ['2.3 - Drag', dragSketch],
-  ['2.6 - Single Draggable Attractor', singleDraggableAttractorSketch],
-  ['2.8 - Many Attractors', manyAttractorsSketch],
-  ['3.2 - Angular Rotation', angularRotationSketch],
-  ['3.2b - Cannonball', cannonballSketch],
-  ['3.5 - Arctan Followers', arctanFollowersSketch],
-  ['4.2 - Particle System', ParticleSystemSketch],
-  ['4.2b - Particle System [COSMIC]', ParticleSystemCosmicSketch],
-  ['4.8 - Smoke', smokeSketch],
-  ['6.1 - Seeking a Target', seekingATargetSketch],
-  ['6.4 - Wanderers', wanderersSketch],
-  ['6.6 - Flow Fields', flowFieldsSketch],
-  ['6.8 - Linear Path Following', linearPathFollowingSketch],
-  ['6.8b - Circular Path Following', circularPathFollowingSketch],
-  ['6.11 - Group Behavior', groupBehaviorSketch],
-  ['6.13 - Flocking', flockingSketch],
-  ['7.4 - 1d Cellular Automation', oneDellularAutomationSketch],
-  ['7.7 - Game of Life', gameOfLifeSketch],
-  ['WIP - spaceship', spaceshipSketch],
-  ['WIP - environment project', environmentSketch],
-  ['xc - Ray Casting', rayCastingSketch],
-  ['xc - Ray Casting Plus', rayCastingPlusSketch],
+  ["0.1 - 1D Collision", oneDimendionalCollisionSketch],
+  ["1 - Mouse Followers", mouseFollowersSketch],
+  ["1a - Triangle Collision", triangleCollisionSketch],
+  ["2.1 - Wind & Gravity", windGravitySketch],
+  ["2.2 - Wind, Gravity & Friction", windGravityFrictionSketch],
+  [
+    "2.2b - Wind, Gravity, Friction, Collision",
+    windGravityFrictionCollisionSketch,
+  ],
+  ["2.3 - Drag", dragSketch],
+  ["2.6 - Single Draggable Attractor", singleDraggableAttractorSketch],
+  ["2.8 - Many Attractors", manyAttractorsSketch],
+  ["3.2 - Angular Rotation", angularRotationSketch],
+  ["3.2b - Cannonball", cannonballSketch],
+  ["3.5 - Arctan Followers", arctanFollowersSketch],
+  ["4.2 - Particle System", ParticleSystemSketch],
+  ["4.2b - Particle System [COSMIC]", ParticleSystemCosmicSketch],
+  ["4.8 - Smoke", smokeSketch],
+  ["6.1 - Seeking a Target", seekingATargetSketch],
+  ["6.4 - Wanderers", wanderersSketch],
+  ["6.6 - Flow Fields", flowFieldsSketch],
+  ["6.8 - Linear Path Following", linearPathFollowingSketch],
+  ["6.8b - Circular Path Following", circularPathFollowingSketch],
+  ["6.11 - Group Behavior", groupBehaviorSketch],
+  ["6.13 - Flocking", flockingSketch],
+  ["7.4 - 1d Cellular Automation", oneDellularAutomationSketch],
+  ["7.7 - Game of Life", gameOfLifeSketch],
+  ["WIP - spaceship", spaceshipSketch],
+  ["WIP - environment project", environmentSketch],
+  ["xc - Ray Casting", rayCastingSketch],
+  ["xc - Ray Casting Plus", rayCastingPlusSketch],
 ];
 
-const sidenav = document.getElementById('sidenav');
-const sidenavList = document.getElementById('sidenavList');
+const sidePanel = document.getElementById("side-panel");
+const sketchList = document.getElementById("side-panel-list");
 
 const createLinkText = (s: string) => {
-  const [,title] = s.match(/- (.*)/);
-  return title.toLowerCase().replace(/ /g, '_');
+  const [, title] = s.match(/- (.*)/);
+  return title.toLowerCase().replace(/ /g, "_");
 };
 
 for (const pair of sketches) {
-  const title = document.createElement('li');
-  const a = document.createElement('a');
+  const title = document.createElement("li");
+  const a = document.createElement("a");
   a.innerText = pair[0];
-  a.href = createLinkText(pair[0]);
+  a.href = `#${createLinkText(pair[0])}`;
   title.appendChild(a);
-  sidenavList.appendChild(title);
+  sketchList.appendChild(title);
 }
 
-const container = document.getElementsByClassName('container')[0] as HTMLElement;
+const container = document.getElementsByClassName(
+  "container"
+)[0] as HTMLElement;
 
-const closeSidenav = document.getElementById('closebtn');
-closeSidenav.onclick = () => {
-  sidenav.style.width = '0';
-  container.style.marginLeft = '0';
+const closeButton = document.getElementById("close-button");
+const closeSidePanel = () => {
+  sidePanel.style.width = "0";
+  container.style.marginLeft = "0";
+  openSidePanel.innerText = `> ${getDisplayTitle()}`;
 };
-const openSidenav = document.getElementById('openSidenav');
-openSidenav.onclick = () => {
-  sidenav.style.width = '30rem';
-  container.style.marginLeft = '30rem';
+closeButton.onclick = () => {
+  closeSidePanel();
+};
+const openSidePanel = document.getElementById("open-side-panel");
+openSidePanel.onclick = () => {
+  sidePanel.style.width = "30rem";
+  container.style.marginLeft = "30rem";
+  openSidePanel.innerText = `< ${getDisplayTitle()}`;
 };
 
-const options = document.getElementsByClassName('options')[0] as HTMLElement;
+document.addEventListener("click", (event) => {
+  const clickedInSidenav = event.target instanceof Node && sidePanel.contains(event.target);
+  const clickedSidenavButton = event.target instanceof Node && openSidePanel.contains(event.target);
 
-const isLocalHost = window.location.hostname === 'localhost';
-let title = window.location.pathname;
-title = title.replace(isLocalHost ? /^\// : /^\/nature-of-code/, '');
+  if (!clickedInSidenav && !clickedSidenavButton) {
+    closeSidePanel();
+  }
+});
+
+const options = document.getElementsByClassName("options")[0] as HTMLElement;
+let p: P5;
+
+const getTitle = () => {
+  let title = window.location.hash;
+  title = title.replace("#", "");
+  return title;
+};
+
+const getDisplayTitle = () => {
+  let title = getTitle().replace(/_/g, " ");
+  if (!title) title = 'select a sketch'
+  return title;
+}
+
+const renderPage = () => {
+  const title = getTitle();
+  if (title) {
+    loadSketch(title);
+    options.style.display = "flex";
+  } else {
+    options.style.display = "none";
+    openSidePanel.innerText = "> select a sketch";
+  }
+};
+
+window.onhashchange = () => {
+  renderPage();
+};
 
 window.onload = () => {
-  if (title) {
-    openSidenav.innerText = `> ${title.replace(/_/g, ' ')}`
-  } else {
-    options.style.display = 'none';
-  }
-}
-let selectedIndex = sketches.findIndex(pair => createLinkText(pair[0]) === title);
-const sketch = selectedIndex > -1 ? sketches[selectedIndex][1] : () => null;
-let p = new P5(sketch, document.getElementById("app"));
+  renderPage();
+};
 
-const resetButton = document.getElementById('reset');
-resetButton.onclick = () => {
-  p.remove();
+const loadSketch = (title: string) => {
+  if (!title) return;
+  openSidePanel.innerText = `> ${title.replace(/_/g, " ")}`;
+  let selectedIndex = sketches.findIndex(
+    (pair) => createLinkText(pair[0]) === title
+  );
+  const sketch = selectedIndex > -1 ? sketches[selectedIndex][1] : () => null;
+  if (p) p.remove();
   p = new P5(sketch, document.getElementById("app"));
-}
+  return p;
+};
+
+const resetButton = document.getElementById("reset");
+resetButton.onclick = () => {
+  if (p) p.remove();
+  p = loadSketch(getTitle());
+};
